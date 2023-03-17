d06b7d8c133805adf74b539d741da5cef2143ba4
zaman <zamanankon2@gmail.com>
2023-03-17 07:58:27 +0600

    counter

---
+++ index.js
@@ -0,0 +1,9 @@
+
+let value1=0;
+let totalCount=setInterval(() => {
+  const count=document.getElementById('count')
+  const value=totalCount;
+  count.innerText=value;
+  totalCount++;
+}, 1000);
+ 
